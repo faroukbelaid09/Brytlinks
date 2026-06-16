04dd7590b11d3a7aafd4359006e10a1225324624
faroukbelaid09 <faroukbelaid9@gmail.com>
2026-06-16 18:26:00 +0300

    Updates

--- _next/static/vBNAcCzLGX6GZ359m_r1-/_buildManifest.js
+++ _next/static/vBNAcCzLGX6GZ359m_r1-/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Brytlinks//_next/:path+",
-        "destination": "/Brytlinks/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
